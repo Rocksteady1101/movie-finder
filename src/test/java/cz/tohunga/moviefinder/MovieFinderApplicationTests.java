1a486572ef63a2d0f974a8195f9e05e309c6c049
Jan Davídek <jan.davidek@tipsport.cz>
2023-11-01 16:46:44 +0100

    initial commit

---
+++ src/test/java/cz/tohunga/moviefinder/MovieFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.tohunga.moviefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
